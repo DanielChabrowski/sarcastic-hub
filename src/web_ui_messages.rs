03e737f8630e208ef1b8f5e6dc3c34e80be352b6
Daniel Chabrowski <danchabrowski@gmail.com>
2020-11-20 01:35:37 +0100

    Initial web ui server

---
+++ src/web_ui_messages.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct QueryProviders {}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct QueryResources {}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Action {
+    command: String,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub enum WebUiRequest {
+    QueryProviders(QueryProviders),
+    QueryResources(QueryResources),
+    Action(Action),
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct Provider {}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct Resource {}
+
+#[derive(Debug, Clone, Serialize)]
+pub enum WebUiResponse {
+    Providers(Vec<Provider>),
+    Resources(Vec<Resource>),
+}
