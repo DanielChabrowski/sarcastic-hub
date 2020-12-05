0adc8dd227abf537a700a38794edf170486a2921
Daniel Chabrowski <danchabrowski@gmail.com>
2020-12-05 12:11:19 +0100

    Split handling of web ui request

--- src/hub.rs
+++ src/hub.rs
@@ -1,4 +1,6 @@
-use crate::web_ui_messages::{ProblemDetails, WebUiRequest, WebUiResponse};
+use crate::web_ui_messages::{
+    Action, ProblemDetails, QueryProviders, QueryResources, WebUiRequest, WebUiResponse,
+};
 
 pub struct Hub {}
 
@@ -7,9 +9,29 @@ impl Hub {
         Self {}
     }
 
-    pub fn handle_web_ui_request(&self, _req: &WebUiRequest) -> WebUiResponse {
+    pub fn handle_web_ui_request(&self, req: &WebUiRequest) -> WebUiResponse {
+        match req {
+            WebUiRequest::QueryProviders(q) => self.handle_query_providers(q),
+            WebUiRequest::QueryResources(q) => self.handle_query_resources(q),
+            WebUiRequest::Action(q) => self.handle_action(q),
+        }
+    }
+
+    fn handle_query_providers(&self, _query: &QueryProviders) -> WebUiResponse {
         WebUiResponse::Error(ProblemDetails {
-            description: "Not implemented".into(),
+            description: "QueryProviders not implemented".into(),
+        })
+    }
+
+    fn handle_query_resources(&self, _query: &QueryResources) -> WebUiResponse {
+        WebUiResponse::Error(ProblemDetails {
+            description: "QueryResources not implemented".into(),
+        })
+    }
+
+    fn handle_action(&self, _query: &Action) -> WebUiResponse {
+        WebUiResponse::Error(ProblemDetails {
+            description: "Action not implemented".into(),
         })
     }
 }
