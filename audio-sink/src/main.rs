8558b0e62f22ca6754228fdffb791b5ab9ae4372
Daniel Chabrowski <danchabrowski@gmail.com>
2021-04-25 16:15:05 +0200

    Refactor PulseAudio interaction

--- audio-sink/src/main.rs
+++ audio-sink/src/main.rs
@@ -1,131 +1,11 @@
-use anyhow::anyhow;
+mod pulsewatcher;
+use pulsewatcher::{PulseMessage, PulseWatcher};
+
 use futures_util::SinkExt;
 use futures_util::StreamExt;
+use gstreamer::prelude::*;
 use log::{debug, error, info};
-use pulse::context::Context;
-use pulse::mainloop::api::Mainloop as MainloopTrait;
-use pulse::mainloop::standard::Mainloop;
-use pulse::{callbacks::ListResult, mainloop::standard::IterateResult};
-use std::{cell::RefCell, ops::Deref, rc::Rc, sync::Arc};
 use tokio::sync::mpsc::unbounded_channel as channel;
-use tokio::sync::mpsc::UnboundedSender as Sender;
-
-#[derive(Debug, PartialEq)]
-enum PAState {
-    Disconnected,
-}
-
-fn connect(sender: Sender<HubMessage>) {
-    debug!("Connecting to PulseAudio");
-
-    let mainloop = Arc::new(RefCell::new(Mainloop::new().expect("Mainloop created")));
-    let inner = Arc::new(mainloop.borrow().inner());
-
-    let ctx = Arc::new(RefCell::new(
-        Context::new(mainloop.borrow().deref(), "ContextName").expect("Context created"),
-    ));
-
-    let _ = ctx
-        .borrow_mut()
-        .connect(None, pulse::context::FlagSet::NOFLAGS, None)
-        .map_err(|e| anyhow!("Err: {:?}", e));
-
-    loop {
-        match mainloop.borrow_mut().iterate(false) {
-            IterateResult::Quit(_) | IterateResult::Err(_) => {
-                error!("Iterate state was not success, quitting...");
-                return;
-            }
-            IterateResult::Success(_) => {}
-        }
-
-        match ctx.borrow().get_state() {
-            pulse::context::State::Ready => {
-                debug!("Connected to PulseAudio");
-                break;
-            }
-            pulse::context::State::Failed | pulse::context::State::Terminated => {
-                error!("Context state failed/terminated, quitting...");
-                return;
-            }
-            _ => {}
-        }
-    }
-
-    use pulse::context::subscribe::InterestMaskSet;
-    ctx.borrow_mut().subscribe(InterestMaskSet::SINK, |_| {});
-
-    let ctx_c = ctx.clone();
-    let sender_c = sender.clone();
-    ctx.borrow_mut().set_state_callback(Some(Box::new(move || {
-        let mut exit = false;
-        let state = ctx_c.borrow().get_state();
-        debug!("State changed to: {:?}", state);
-
-        use pulse::context::State;
-        let hub_message = if let State::Failed | State::Terminated = state {
-            exit = true;
-            Some(HubMessage::StateChanged(PAState::Disconnected))
-        } else {
-            None
-        };
-
-        let sender_c = sender_c.clone();
-        if let Some(msg) = hub_message {
-            sender_c.send(msg).expect("msg");
-        };
-
-        if exit {
-            let mut ml = Mainloop {
-                _inner: inner.as_ref().clone(),
-            };
-            ml.quit(pulse::def::Retval(0));
-        }
-    })));
-
-    let ctx_c = ctx.clone();
-    let sender_c = sender.clone();
-    ctx.borrow_mut()
-        .set_subscribe_callback(Some(Box::new(move |_, op, _| {
-            if op == Some(pulse::context::subscribe::Operation::Changed) {
-                return;
-            }
-
-            debug!("Subscription: {:?}", op);
-
-            let sinks = Rc::new(RefCell::new(vec![]));
-            let sender = sender_c.clone();
-            ctx_c
-                .borrow_mut()
-                .introspect()
-                .get_sink_info_list(move |result| match result {
-                    ListResult::Item(item) => {
-                        let sink_desc = item.description.as_ref().map(|cow| cow.to_string());
-                        if let Some(desc) = sink_desc {
-                            sinks.borrow_mut().push(desc);
-                        }
-                    }
-                    ListResult::End => {
-                        let sinks = sinks.borrow_mut().clone();
-                        sender
-                            .send(HubMessage::OutputDevices(sinks))
-                            .expect("OutputDevices sent");
-                    }
-                    _ => {
-                        // Error
-                    }
-                });
-        })));
-
-    let _ = mainloop.borrow_mut().run();
-    info!("Receiver ended");
-}
-
-#[derive(Debug)]
-enum HubMessage {
-    StateChanged(PAState),
-    OutputDevices(Vec<String>),
-}
 
 #[tokio::main]
 async fn main() {
@@ -141,7 +21,43 @@ async fn main() {
         env!("CARGO_PKG_VERSION")
     );
 
-    let (sender, mut receiver) = channel::<HubMessage>();
+    let player = gstreamer_player::Player::new(None, None);
+    player.connect_error(|_, err| {
+        error!("{:?}", err);
+    });
+
+    player.connect_state_changed(|p, _err| {
+        debug!("state changed: {:?}", p);
+    });
+
+    player.connect_buffering(|_, p| {
+        debug!("buffering {}", p);
+    });
+
+    use glib::GString;
+
+    let pipeline = player.get_pipeline();
+
+    let pulsesink = gstreamer::ElementFactory::make("pulsesink", Some("dantesink")).unwrap();
+
+    pulsesink.connect_notify(Some("current-device"), |zelf, _| {
+        let current_device = zelf.get_property("current-device").unwrap();
+        debug!(
+            "Current device: {:?}",
+            current_device.get::<GString>().unwrap().unwrap().as_str()
+        );
+    });
+
+    pipeline.set_property("audio-sink", &pulsesink).unwrap();
+
+    player.play();
+
+    let (sender, mut receiver) = channel::<PulseMessage>();
+
+    let pulse_audio_connection = tokio::task::spawn_blocking(move || {
+        let pulse_watcher = PulseWatcher::new(sender).unwrap();
+        pulse_watcher.run().unwrap()
+    });
 
     let server = tokio::spawn(async move {
         while let Some(msg) = receiver.recv().await {
@@ -169,8 +85,6 @@ async fn main() {
         ))
         .await
         .expect("Message send");
-
-    let pulse_audio_connection = tokio::task::spawn_blocking(move || connect(sender));
 
     tokio::try_join!(server, hub_connection, pulse_audio_connection).expect("Joined");
 }
---
+++ audio-sink/src/pulsewatcher.rs
@@ -0,0 +1,185 @@
+use anyhow::{anyhow, Result};
+use log::{debug, error};
+use pulse::{callbacks::ListResult, context::subscribe::InterestMaskSet};
+use pulse::{context::Context, mainloop::api::Mainloop as MainloopTrait};
+use pulse::{
+    context::State,
+    mainloop::standard::{IterateResult, Mainloop},
+};
+use std::{cell::RefCell, ops::Deref, rc::Rc, sync::Arc};
+use tokio::sync::mpsc::UnboundedSender as Sender;
+
+type PulseSender = Sender<PulseMessage>;
+
+#[derive(Debug, PartialEq)]
+pub enum PulseState {
+    Connected,
+    Disconnected,
+}
+
+#[derive(Debug)]
+pub enum PulseMessage {
+    StateChanged(PulseState),
+    SinksUpdated(Vec<String>),
+}
+
+pub struct PulseWatcher {
+    mainloop: Arc<RefCell<Mainloop>>,
+    context: Arc<RefCell<Context>>,
+    sender: PulseSender,
+}
+
+impl PulseWatcher {
+    pub fn new(sender: PulseSender) -> Result<PulseWatcher> {
+        let mainloop =
+            Arc::new(RefCell::new(Mainloop::new().ok_or_else(|| {
+                anyhow!("Could not create a PulseAudio mainloop")
+            })?));
+
+        let context = Arc::new(RefCell::new(
+            Context::new(mainloop.borrow().deref(), "ContextName")
+                .ok_or_else(|| anyhow!("Could not create a PulseAudio context"))?,
+        ));
+
+        Ok(PulseWatcher {
+            mainloop,
+            context,
+            sender,
+        })
+    }
+
+    pub fn run(&self) -> Result<()> {
+        debug!("Starting PulseWatcher");
+
+        self.connect()?;
+        self.subscribe_to_state_changes();
+        self.subscribe_to_events();
+
+        self.mainloop.borrow_mut().run().map_err(|(e, ret_val)| {
+            anyhow!(
+                "Cannot run PulseAudio loop, err: {}, ret_val: {:?}",
+                e,
+                ret_val
+            )
+        })?;
+
+        Ok(())
+    }
+
+    fn connect(&self) -> Result<()> {
+        debug!("Trying to connect to PulseAudio server");
+
+        self.context
+            .borrow_mut()
+            .connect(None, pulse::context::FlagSet::NOAUTOSPAWN, None)?;
+
+        loop {
+            // debug!("Loop!");
+            match self.mainloop.borrow_mut().iterate(false) {
+                IterateResult::Quit(e) => {
+                    return Err(anyhow!("Return value while connecting: {:?}", e));
+                }
+                IterateResult::Err(e) => {
+                    return Err(anyhow!("Error while connecting: {:?}", e));
+                }
+                IterateResult::Success(_) => {}
+            }
+
+            match self.context.borrow().get_state() {
+                pulse::context::State::Ready => {
+                    debug!("Connected to PulseAudio");
+                    if let Err(e) = self
+                        .sender
+                        .send(PulseMessage::StateChanged(PulseState::Connected))
+                    {
+                        error!("Error while sending state update: {:?}", e);
+                    }
+                    break;
+                }
+                pulse::context::State::Failed | pulse::context::State::Terminated => {
+                    return Err(anyhow!(
+                        "Context state failed or terminated after connecting, quitting."
+                    ));
+                }
+                _ => {}
+            }
+        }
+
+        Ok(())
+    }
+
+    fn subscribe_to_state_changes(&self) {
+        let inner = Arc::new(self.mainloop.borrow().inner());
+        let ctx = self.context.clone();
+        let sender = self.sender.clone();
+
+        self.context
+            .borrow_mut()
+            .set_state_callback(Some(Box::new(move || {
+                let mut exit = false;
+                let state = ctx.borrow().get_state();
+                debug!("State changed to: {:?}", state);
+
+                let message = if let State::Failed | State::Terminated = state {
+                    exit = true;
+                    Some(PulseMessage::StateChanged(PulseState::Disconnected))
+                } else {
+                    None
+                };
+
+                if let Some(msg) = message {
+                    if let Err(e) = sender.send(msg) {
+                        error!("Error while sending state update: {:?}", e);
+                    }
+                };
+
+                if exit {
+                    // Awful hack
+                    let mut ml = Mainloop {
+                        _inner: inner.as_ref().clone(),
+                    };
+                    ml.quit(pulse::def::Retval(0));
+                }
+            })));
+    }
+
+    fn subscribe_to_events(&self) {
+        debug!(target: "PulseWatcher", "Subscribing to events");
+
+        let sender = self.sender.clone();
+        let ctx = self.context.clone();
+        let callback = move |_, op, _| {
+            if op == Some(pulse::context::subscribe::Operation::Changed) {
+                return;
+            }
+
+            debug!("Subscribed event notification");
+
+            let sinks = Rc::new(RefCell::new(vec![]));
+            let sender = sender.clone();
+            ctx.borrow_mut()
+                .introspect()
+                .get_sink_info_list(move |result| match result {
+                    ListResult::Item(item) => {
+                        let sink_desc = item.description.as_ref().map(|cow| cow.to_string());
+                        if let Some(desc) = sink_desc {
+                            sinks.borrow_mut().push(desc);
+                        }
+                    }
+                    ListResult::End => {
+                        let sinks = sinks.borrow_mut().clone();
+                        if let Err(e) = sender.send(PulseMessage::SinksUpdated(sinks)) {
+                            error!("Error while sending sink update: {:?}", e);
+                        }
+                    }
+                    ListResult::Error => {
+                        error!("Error while retrieving sinks");
+                    }
+                });
+        };
+
+        let mut ctx = self.context.borrow_mut();
+        ctx.subscribe(InterestMaskSet::SINK, |_| {});
+        ctx.set_subscribe_callback(Some(Box::new(callback)));
+    }
+}
